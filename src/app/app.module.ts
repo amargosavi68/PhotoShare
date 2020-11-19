01c3814ecaab3ed1a9261f25f2a649af7df6d590
Amar Gosavi <gosaviamar681999@gmail.com>
2020-11-19 19:27:01 +0530

    home and photoUpload components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatCardModule } from "@angular/material/card";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatButtonModule } from "@angular/material/button";
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+import 'hammerjs';
+import { HomeComponent } from './home/home.component';
+import { UploadphotoComponent } from './uploadphoto/uploadphoto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    UploadphotoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatButtonModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
