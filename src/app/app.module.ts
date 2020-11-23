b99c2e08563c50590cabbc13196d3a060b173bed
Amar Gosavi <gosaviamar681999@gmail.com>
2020-11-23 14:25:53 +0530

    login component and form designed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatCardModule } from "@angular/material/card";
 import { MatListModule } from "@angular/material/list";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { FormsModule } from "@angular/forms";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { MatDialogModule } from "@angular/material/dialog";
+import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
+
 
 import 'hammerjs';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { UploadphotoComponent } from './uploadphoto/uploadphoto.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+
 
 @NgModule({
   declarations: [
@@ -38,11 +42,16 @@ import { SignupComponent } from './signup/signup.component';
     MatListModule,
     MatSidenavModule,
     MatCardModule,
-    FormsModule,
     MatFormFieldModule,
+    MatDialogModule,
     MatInputModule,
     MatButtonModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  entryComponents:[
+    LoginComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
