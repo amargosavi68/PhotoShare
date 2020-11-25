61d86f2b47a1d2ac0c187f47310fd7d9c4ceccfb
Amar Gosavi <gosaviamar681999@gmail.com>
2020-11-25 13:38:01 +0530

    uploading photo form updated with reactive form

---
+++ src/app/shared/upload.ts
@@ -0,0 +1,5 @@
+export class Formdata {
+     'title': string;
+     'image': string;
+     'description': string;
+}
--- src/app/uploadphoto/uploadphoto.component.ts
+++ src/app/uploadphoto/uploadphoto.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { PhotosService } from '../services/photos.service';
+import { Formdata } from "../shared/upload";
 
 @Component({
   selector: 'app-uploadphoto',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadphotoComponent implements OnInit {
 
-  constructor() { }
+  'uploadForm': FormGroup;
+  'formData': Formdata;
+  @ViewChild('uform') uploadFormDirective:any;
+
+  constructor(private fb: FormBuilder, private photoService: PhotosService) { 
+    this.createForm();
+  }
 
   ngOnInit(): void {
   }
 
+  createForm() {
+    this.uploadForm = this.fb.group({
+      title: '',
+      image: '',
+      description: ''
+    });
+  }
+
+  onSubmit() {
+    this.formData = this.uploadForm.value;
+    console.log(this.formData);
+    this.uploadForm.reset({
+      title:'',
+      image:'',
+      description:'',
+    });
+    this.uploadFormDirective.resetForm();
+  }
 }
